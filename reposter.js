171aad200cbedf386059f4afc2237a57bde04a1d
Rayne <saatviknagpal1234@gmail.com>
2020-08-11 13:14:15 +0530

    ggfg

--- reposter.js
+++ reposter.js
@@ -482,6 +482,7 @@ function sendCommands(channel) {
 client.on("message", function(message) {
 	repostLive(message);
 	if (message.author.bot) return;
+	if (!message.member.hasPermission("ADMINISTRATOR")) return;
 	const args = message.content.toLowerCase().split(" ");
 	const prefix = config.prefixes[(message.guild || message.channel).id] || "/";
 	if (args[0].startsWith(prefix + "repost")) {
